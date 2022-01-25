c8506b9fde70f7890c4bc95995c64880f40246d2
Antonio Muñoz <antonio.munoz@clarity.ai>
2022-01-25 11:48:33 +0100

    more java17 improvements

--- src/main/java/com/github/tonivade/puredbc/RowMetaData.java
+++ src/main/java/com/github/tonivade/puredbc/RowMetaData.java
@@ -5,8 +5,6 @@
 package com.github.tonivade.puredbc;
 
 import static com.github.tonivade.purefun.Precondition.checkNonNull;
-import java.util.Objects;
-import com.github.tonivade.purefun.Equal;
 import com.github.tonivade.purefun.type.Option;
 
 public interface RowMetaData {
@@ -21,47 +19,11 @@ public interface RowMetaData {
   
   Option<ColumnMetaData> column(int index);
 
-  final class ColumnMetaData {
+  record ColumnMetaData(String name, Class<?> type, Boolean nullable) {
 
-    private static final Equal<ColumnMetaData> EQUAL = 
-      Equal.<ColumnMetaData>of()
-        .comparing(ColumnMetaData::name).comparing(ColumnMetaData::type).comparing(x -> x.nullable);
-
-    private final String name;
-    private final Class<?> type;
-    private final Boolean nullable;
-
-    protected ColumnMetaData(String name, Class<?> type, Boolean nullable) {
-      this.name = checkNonNull(name);
-      this.type = checkNonNull(type);
-      this.nullable = nullable;
-    }
-
-    public String name() {
-      return name;
-    }
-
-    public Class<?> type() {
-      return type;
-    }
-
-    public Option<Boolean> isNullable() {
-      return Option.of(nullable);
-    }
-    
-    @Override
-    public int hashCode() {
-      return Objects.hash(name, type, nullable);
-    }
-    
-    @Override
-    public boolean equals(Object obj) {
-      return EQUAL.applyTo(this, obj);
-    }
-
-    @Override
-    public String toString() {
-      return String.format("ColumnMetaData [name=%s, type=%s]", name, type);
+    public ColumnMetaData {
+      checkNonNull(name);
+      checkNonNull(type);
     }
   }
 }
